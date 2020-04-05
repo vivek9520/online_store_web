c94d792c5cdac2e43318e0a99a275602c20c211b
mac <vivekjeevanarajh@gmail.com>
2020-04-05 15:32:01 +0530

    Project Init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+   <div>
+     <div className="jumbotron">
+
+     </div>
+
+   </div>
+  );
+}
+
+export default App;
