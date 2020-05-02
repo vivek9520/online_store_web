014861d08387b242c79a04410f2408494278d545
AsviniSri0721 <asvinisrinivasan21@gmail.com>
2020-05-02 13:18:02 +0530

    My First Commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import VkViewProducts from "./Components/VK_Components/VK_ViewProducts";
-
+import AsAddProduct from "./Components/AS_Components/AS_AddProduct";
+import AsProductList from "./Components/AS_Components/AS_ProductList";
 function App() {
   return (
    <div>
      <div className="jumbotron">
 
      </div>
-        <VkViewProducts/>
+
+        <AsAddProduct/>
+        <AsProductList/>
+
    </div>
   );
 }
