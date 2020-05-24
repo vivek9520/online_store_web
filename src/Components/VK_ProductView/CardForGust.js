772a0e3effd73e3afe2cc8ba94c7f0e3806b5fbb
mac <vivekjeevanarajh@gmail.com>
2020-05-24 18:10:32 +0530

    With Asvini components with some new 6

--- src/Components/VK_ProductView/CardForGust.js
+++ src/Components/VK_ProductView/CardForGust.js
@@ -1,11 +1,19 @@
 import React, {Component, useState} from 'react';
 import img from '../Img/1.jpeg'
 import './CSS/ProductCard.css'
+import {Collapse} from "antd";
+import './CSS/GustCard.css'
 
 
 import {
     Card, CardImg, CardText, CardGroup, CardBody
 } from 'reactstrap';
+import SingleAddToCart from "./Section/SingleAddToCart";
+import ProductDetails from "./ProductDetails";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const {Panel} =Collapse;
 
 
 class CardForGust extends Component {
@@ -21,21 +29,44 @@ class CardForGust extends Component {
     }
 
 
+
+
+
+
+
+
     render() {
 
         return (
-            <section className="tc pa5 pa5-ns wl"  height="140"
-                     width="180">
+            <section className="tc pa5 pa5-ns wl card1"  height="140"
+                     width="300">
                 <article className="hide-child relative ba b--black-10 mw5 center">
                     <img
                         src={`/uploads/${this.props.productData.filename}` }
                         className="db" alt={this.props.productData.filename}  height="140"
                         width="180"/>
                     <div className="pa2 bt b--black-20">
-                        <a className="f6 db link dark-blue hover-blue" >{this.props.productData.productName}</a>
+                        <b> <a className="f6 db link dark-blue hover-blue" >{this.props.productData.productName}</a>
                         <a className="f6 db link dark-blue hover-blue" >{this.props.productData.size}</a>
-                        <p className="f6 gray mv1">Rs/{this.props.productData.price}</p>
-                        <a className="link tc ph3 pv1 db bg-animate bg-dark-blue hover-bg-blue white f6 br1" style={{color:"white" ,margin:"2px"}} >View</a>
+                      <p className="f6 gray mv1">Rs/{this.props.productData.price}</p></b>
+
+                    <Collapse defaultActiveKey={['0']}>
+                            <Panel  header="View" key="1" style={{ color:"red"}}>
+                            <b>    <p>{this.props.productData.productName}</p>
+                                <p>{this.props.productData.size}</p>
+                                <p>{this.props.productData.price}</p>
+                                <p>{this.props.productData.color}</p>
+                                <p>{this.props.productData.cateName}</p>
+                                <p style={{color:"green"}}><b>- {this.props.productData.discount}</b></p>
+                                <p><Collapse defaultActiveKey={['0']}>
+                                    <Panel  header="description" key="1" style={{ color:"red" }}>
+                                        <p>{this.props.productData.description}</p>
+
+                                    </Panel>
+                                </Collapse></p>
+                            </b>
+                            </Panel>
+                        </Collapse>
 
                     </div>
 
--- src/Components/VK_ProductView/ProductCardView.js
+++ src/Components/VK_ProductView/ProductCardView.js
@@ -2,12 +2,15 @@ import React, {Component, useState} from 'react';
 import img from '../Img/1.jpeg'
 import './CSS/ProductCard.css'
 import ProductDetails from "./ProductDetails";
+import "./CSS/GustCard.css"
 
 
 import {
     Card, CardImg, CardText, CardGroup, CardBody
 } from 'reactstrap';
 import axios from "axios";
+import {Collapse} from "antd";
+const {Panel} =Collapse;
 
 
 class ProductCardView extends Component {
@@ -55,7 +58,7 @@ class ProductCardView extends Component {
     render() {
 
         return (
-            <section className="tc pa5 pa5-ns wl"  height="140"
+            <section className="tc pa5 pa5-ns wl card1"  height="140"
                      width="180">
                 <article className="hide-child relative ba b--black-10 mw5 center">
                     <img
@@ -67,10 +70,27 @@ class ProductCardView extends Component {
                         <a className="f6 db link dark-blue hover-blue" >{this.props.productData.size}</a>
 
                         <p className="f6 gray mv1">Rs/{this.props.productData.price}</p>
-                        <a className="link tc ph3 pv1 db bg-animate bg-dark-blue hover-bg-blue white f6 br1" style={{color:"white" ,margin:"2px" }} onClick={()=>this.viewDetails(this.props.productData)}>View</a>
+
                         <a className="link tc ph3 pv1 db bg-animate bg-dark-blue hover-bg-blue white f6 br1" style={{color:"white" ,margin:"2px"}} onClick={()=>this.props.addcart(this.props.productData)}>Add  Cart</a>
                         <a className="link tc ph3 pv1 db bg-animate bg-dark-blue hover-bg-blue white f6 br1" style={{color:"white" ,margin:"2px" }} onClick={()=>this.props.addtoWish(this.props.productData)}>Add  Wish</a>
 
+                        <Collapse defaultActiveKey={['0']}>
+                            <Panel  header="View" key="1" style={{ color:"red"}}>
+                                <b>    <p>{this.props.productData.productName}</p>
+                                    <p>{this.props.productData.size}</p>
+                                    <p>{this.props.productData.price}</p>
+                                    <p>{this.props.productData.color}</p>
+                                    <p>{this.props.productData.cateName}</p>
+                                    <p style={{color:"green"}}><b>- {this.props.productData.discount}</b></p>
+                                    <p><Collapse defaultActiveKey={['0']}>
+                                        <Panel  header="description" key="1" style={{ color:"red" }}>
+                                            <p>{this.props.productData.description}</p>
+
+                                        </Panel>
+                                    </Collapse></p>
+                                </b>
+                            </Panel>
+                        </Collapse>
 
                     </div>
 
